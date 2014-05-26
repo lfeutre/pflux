480221e3895520c82554c5616592ae82685ba36e
Duncan McGreggor <duncan@cogitat.io>
2014-05-25 21:11:13 -0700

    Added yaws make include.

---
+++ resources/make/yaws.mk
@@ -0,0 +1,25 @@
+ETC_DIR = ./etc
+YAWS_DIR = $(DEPS)/yaws
+YAWS = $(YAWS_DIR)/bin/yaws
+YAWS_CONF = $(ETC_DIR)/yaws.conf
+YAWS_SERVER_ID = pflux
+
+app-deps:
+	@mkdir -p logs
+
+dev: app-deps compile-no-deps
+	@ERL_LIBS=$(shell lfetool info erllibs) \
+	$(YAWS) -i --conf $(YAWS_CONF) --id $(YAWS_SERVER_ID)
+
+run: app-deps compile
+	@ERL_LIBS=$(shell lfetool info erllibs) \
+	$(YAWS) -D --heart --conf $(YAWS_CONF) --id $(YAWS_SERVER_ID)
+
+update-conf:
+	@ERL_LIBS=$(ERL_LIBS) $(YAWS) -h --conf $(YAWS_CONF) --id $(YAWS_SERVER_ID)
+
+stats:
+	@ERL_LIBS=$(ERL_LIBS) $(YAWS) -S --id $(YAWS_SERVER_ID)
+
+stop:
+	@ERL_LIBS=$(ERL_LIBS) $(YAWS) --stop --id $(YAWS_SERVER_ID)
